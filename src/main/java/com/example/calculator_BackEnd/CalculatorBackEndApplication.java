2ea4832f7f46c2c11295338a354c0bff7b19d8f0
Austin Durkin <infoAustinDurkin@gmail.com>
2024-12-16 16:22:53 +0000

    Initial commit

---
+++ src/main/java/com/example/calculator_BackEnd/CalculatorBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculator_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calculator_BackEnd/CalculatorBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculator_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
